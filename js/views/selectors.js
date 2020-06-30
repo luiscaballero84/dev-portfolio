75f2d6b17e360aec63b5d4272141b6791f9c87b5
Luis Caballero <mendoza386@gmail.com>
2020-06-30 21:51:30 +0200

    changed all layout to make the nav-sticky works

--- js/views/selectors.js
+++ js/views/selectors.js
@@ -3,6 +3,12 @@ console.log("selectors is connected");
 export const elements = {
   burgerIcon: document.querySelector('.navbar-burger'),
   navMenu: document.querySelector('.navbar-menu'),
+  projectsBtn: document.getElementById('project-btn'),
+  aboutBtn: document.getElementById('about-btn')
+};
+
+export const anchors = {
+  projects: document.getElementById('projects-section')
 };
 
 console.log(elements.burgerIcon);
