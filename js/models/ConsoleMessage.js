46937e4e4d023158be8650cbf20588b2352b8004
Luis <mendoza386@gmail.com>
2022-04-18 16:15:38 +0200

    added console.log message about typeErorr

--- js/models/ConsoleMessage.js
+++ js/models/ConsoleMessage.js
@@ -3,5 +3,6 @@
 export function consoleMessages() {
   console.log("Hello! 🙂 ")
   console.log("Website designed and coded by Luis Caballero.")
+  console.log("Yes, I know, there is an Uncaught TypeError. I am working to solve this. Page works fine, though 👌")
 }
 
