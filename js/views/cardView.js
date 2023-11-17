f248b746ff060fe1384887acc897dfb819ef09be
Luis <mendoza386@gmail.com>
2023-11-17 11:01:33 +0100

    new project cards done

--- js/views/cardView.js
+++ js/views/cardView.js
@@ -1,35 +1,48 @@
 const projectList = [
   {
     name: "Raus Companion",
-    text: "Building the perfect helper for Raus guests.",
-    img: "img/projects/project-companion-img-00-thumb-2x.jpg",
+    text: "Reducing Customer Service requests with the first in-house product.",
+    img: "img/projects/project-companion-img-00-thumb-2x.png",
     alt: "raus companion project image",
     link: "pages/case-companion.html",
-    firstImgClass:"js-first-img"
+    firstImgClass:"js-first-img",
+    chip01: "UX",
+    chip02: "UI",
+    chip03: "Design system"
   },
   {
     name: "Raus design assets",
-    text: "Laying the Foundation for Raus design system.",
-    img: "img/projects/project-design-system--img00-thumb.jpg",
+    text: "Laying the foundation for Raus design system.",
+    img: "img/projects/project-design-system--img00-thumb.png",
     alt: "raus design assets image",
     link: "pages/case-design-system.html",
-    firstImgClass:""
+    firstImgClass:"",
+    chip01: "UI",
+    chip02: "Figma",
+    chip03: "Design system",
+    
   },
   {
-    name: "tink Blog Redesign",
-    text: "A brand new look for the tink blog. ",
-    img: "img/projects/project-blog-thumb-2x.jpg",
+    name: "tink Blog",
+    text: "Redesign of the company blog,  leading to a 23% increase in readers.",
+    img: "img/projects/project-blog-thumb-2x.png",
     alt: "tink blog redesign project image",
     link: "pages/case-blog.html",
-    firstImgClass:""
+    firstImgClass:"",
+    chip01: "UX",
+    chip02: "UI",
+    chip03: "Branding",
   },
   {
-    name: "Personal Branding",
-    text: "From strategy and content to web development.",
-    img: "img/projects/project-branding-thumb-2x.jpg",
+    name: "Personal portfolio 2022",
+    text: "0 to 1 product: from strategy to web development with great results. ",
+    img: "img/projects/project-branding-thumb-2x.png",
     alt: "Personal bramding project image",
     link: "pages/case-branding.html",
-    firstImgClass:""
+    firstImgClass:"",
+    chip01: "Strategy",
+    chip02: "Branding",
+    chip03: "Web development"
   },
 ];
 
@@ -65,32 +78,78 @@ const projectList = [
 //   }).join('');
 
 //function without lazy load 
+// function renderProjects() {
+//   const cardMarkup = projectList.map(function(project) {  
+
+//     return `
+//       <a class="card" href="${project.link}">
+//           <img 
+//             class="card__img" 
+//             src="${project.img}" 
+//             alt="${project.alt}"
+//           >
+//           <h3 class="card__title">${project.name}</h3>
+//           <p class="card__text">${project.text}</p>
+//           <p class="card__link">Read case study 
+//             <svg class="ui-icon" width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//               <rect x="2" y="11" width="20" height="2" fill="#161616"/>
+//               <rect x="18" y="9" width="2" height="2" fill="#161616"/>
+//               <rect x="18" y="13" width="2" height="2" fill="#161616"/>
+//               <rect x="16" y="15" width="2" height="2" fill="#161616"/>
+//               <rect x="14" y="17" width="2" height="2" fill="#161616"/>
+//               <rect x="16" y="7" width="2" height="2" fill="#161616"/>
+//               <rect x="14" y="5" width="2" height="2" fill="#161616"/>
+//             </svg>
+//           </p>
+//       </a>
+//     `
+//   }).join('');
+
+//function with new card design
 function renderProjects() {
   const cardMarkup = projectList.map(function(project) {  
-
     return `
       <a class="card" href="${project.link}">
-          <img 
-            class="card__img" 
-            src="${project.img}" 
-            alt="${project.alt}"
-          >
-          <h3 class="card__title">${project.name}</h3>
-          <p class="card__text">${project.text}</p>
-          <p class="card__link">Read case study 
-            <svg class="ui-icon" width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect x="2" y="11" width="20" height="2" fill="#161616"/>
-              <rect x="18" y="9" width="2" height="2" fill="#161616"/>
-              <rect x="18" y="13" width="2" height="2" fill="#161616"/>
-              <rect x="16" y="15" width="2" height="2" fill="#161616"/>
-              <rect x="14" y="17" width="2" height="2" fill="#161616"/>
-              <rect x="16" y="7" width="2" height="2" fill="#161616"/>
-              <rect x="14" y="5" width="2" height="2" fill="#161616"/>
+        <img
+          class="card__img"
+          src="${project.img}"
+          alt="${project.alt}"
+        />
+        <p class="card__title txt-upcase txt-x-small">
+        ${project.name}
+        </p>
+        <h3 class="card__text">
+        ${project.text}
+        </h3>
+        <div class="card__chip-container flex flex-center--v">
+          <div class="flex flex--wrap">
+            <span class="chip chip--small">${project.chip01}</span>
+            <span class="chip chip--small">${project.chip02}</span>
+            <span class="chip chip--small">${project.chip03}</span>
+          </div>
+          <div class="card-btn">
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect x="2" y="11" width="20" height="2" fill="#161616" />
+              <rect x="18" y="9" width="2" height="2" fill="#161616" />
+              <rect x="18" y="13" width="2" height="2" fill="#161616" />
+              <rect x="16" y="15" width="2" height="2" fill="#161616" />
+              <rect x="14" y="17" width="2" height="2" fill="#161616" />
+              <rect x="16" y="7" width="2" height="2" fill="#161616" />
+              <rect x="14" y="5" width="2" height="2" fill="#161616" />
             </svg>
-          </p>
+          </div>
+        </div>
       </a>
-    `
+            `
   }).join('');
+
+
 
   const projects = document.querySelector(".js-project");  
 
